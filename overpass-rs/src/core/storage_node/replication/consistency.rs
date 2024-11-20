5419b591b86b6c0a9b50e82081574e768ac42dd5
Cryptskii <47649969+cryptskii@users.noreply.github.com>
2024-11-19 23:47:46 -0500

    Update consistency.rs

--- overpass-rs/src/core/storage_node/replication/consistency.rs
+++ overpass-rs/src/core/storage_node/replication/consistency.rs
@@ -1,11 +1,10 @@
 use std::sync::Arc;
 use parking_lot::RwLock;
 use serde::{Serialize, Deserialize};
-
 use crate::core::error::errors::{SystemError, SystemErrorType};
-use crate::core::replication::proof::ProofManager;
-use crate::core::replication::state::StateManager;
 use crate::core::types::boc::BOC;
+
+use super::state::StateManager;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ConsistencyMetrics {
@@ -28,13 +27,16 @@ impl Default for ConsistencyMetrics {
     }
 }
 
-pub struct ConsistencyValidator {
+pub struct ConsistencyValidator<ProofManager> {
     state_manager: Arc<StateManager>,
     proof_manager: Arc<ProofManager>,
     metrics: RwLock<ConsistencyMetrics>,
 }
 
-impl ConsistencyValidator {
+impl<ProofManager> ConsistencyValidator<ProofManager>
+where
+    ProofManager: VerifyProof,
+{
     pub fn new(state_manager: Arc<StateManager>, proof_manager: Arc<ProofManager>) -> Self {
         Self {
             state_manager,
@@ -56,13 +58,13 @@ impl ConsistencyValidator {
         if !self.state_manager.verify_state(state_hash) {
             self.metrics.write().state_mismatches += 1;
             return Err(SystemError::new(
-                SystemErrorType::StateMismatch,
+                SystemErrorType::InvalidState,
                 "State hash does not exist in state manager".to_owned(),
             ));
         }
 
         // Generate proof inputs
-        let public_inputs = vec![state_hash];
+        let public_inputs = vec![state_hash.to_vec()];
         let proof_result = self.proof_manager.verify_proof(proof, &public_inputs);
 
         match proof_result {
@@ -112,6 +114,10 @@ impl ConsistencyValidator {
     }
 }
 
+pub trait VerifyProof {
+    fn verify_proof(&self, proof: &[u8], public_inputs: &[Vec<u8>]) -> Result<bool, SystemError>;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -123,19 +129,37 @@ mod tests {
         BOC {
             cells: vec![],
             references: vec![],
-            roots: todo!(),
+            roots: vec![],
+        }
+    }
+
+    struct MockProofManager;
+
+    impl MockProofManager {
+        fn new() -> Self {
+            MockProofManager
+        }
+
+        fn generate_proof(&self, _state: &BOC, _public_inputs: &[Vec<u8>]) -> Result<Vec<u8>, SystemError> {
+            Ok(vec![])
+        }
+    }
+
+    impl VerifyProof for MockProofManager {
+        fn verify_proof(&self, _proof: &[u8], _public_inputs: &[Vec<u8>]) -> Result<bool, SystemError> {
+            Ok(true)
         }
     }
 
     #[wasm_bindgen_test]
     async fn test_consistency_validation() {
         let state_manager = Arc::new(StateManager::new().unwrap());
-        let proof_manager = Arc::new(ProofManager::new());
+        let proof_manager = Arc::new(MockProofManager::new());
         let validator = ConsistencyValidator::new(state_manager.clone(), proof_manager.clone());
 
         let state = create_test_boc();
         let state_hash = state_manager.update_wallet_state([1u8; 32], state.clone()).await.unwrap();
-        let public_inputs = vec![state_hash];
+        let public_inputs = vec![state_hash.to_vec()];
 
         let proof = proof_manager.generate_proof(&state, &public_inputs).unwrap();
 
@@ -152,14 +176,14 @@ mod tests {
     #[wasm_bindgen_test]
     async fn test_invalid_consistency() {
         let state_manager = Arc::new(StateManager::new().unwrap());
-        let proof_manager = Arc::new(ProofManager::new());
+        let proof_manager = Arc::new(MockProofManager::new());
         let validator = ConsistencyValidator::new(state_manager.clone(), proof_manager.clone());
 
         let state = create_test_boc();
         let invalid_state = create_test_boc();
 
         let state_hash = state_manager.update_wallet_state([2u8; 32], state.clone()).await.unwrap();
-        let public_inputs = vec![state_hash];
+        let public_inputs = vec![state_hash.to_vec()];
 
         let proof = proof_manager.generate_proof(&state, &public_inputs).unwrap();
 
@@ -177,12 +201,12 @@ mod tests {
     #[wasm_bindgen_test]
     async fn test_consistency_report() {
         let state_manager = Arc::new(StateManager::new().unwrap());
-        let proof_manager = Arc::new(ProofManager::new());
+        let proof_manager = Arc::new(MockProofManager::new());
         let validator = ConsistencyValidator::new(state_manager.clone(), proof_manager.clone());
 
         let state = create_test_boc();
         let state_hash = state_manager.update_wallet_state([3u8; 32], state.clone()).await.unwrap();
-        let public_inputs = vec![state_hash];
+        let public_inputs = vec![state_hash.to_vec()];
 
         let proof = proof_manager.generate_proof(&state, &public_inputs).unwrap();
         validator
