bd6692220533175f15b7410efd895195896b2430
Cryptskii <47649969+cryptskii@users.noreply.github.com>
2024-11-19 22:34:44 -0500

    Update mod.rs

--- overpass-rs/src/core/storage_node/attest/mod.rs
+++ overpass-rs/src/core/storage_node/attest/mod.rs
@@ -3,7 +3,4 @@ pub mod challenge;
 pub mod response;
 pub mod storage_and_retreival;
 
-// re-exporting the modules
-pub use challenge::ChallengeManager;
 
-pub use storage_and_retreival::StorageAndRetrievalManager;
